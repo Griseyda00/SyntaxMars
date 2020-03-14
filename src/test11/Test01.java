0a8f7794e0c0b12afbbdc6c3c3f56e9afbc68568
Griseyda00 <griseyda.09@gmail.com>
2020-03-13 22:35:28 -0400

    one more class added

--- src/test11/Test01.java
+++ src/test11/Test01.java
@@ -1,5 +1,12 @@
 package test11;
 
 public class Test01 {
+	public static void main(String [] args){
+	    for(int i=1;i<=5;i++){
+	      for(int x=1;x<=10;x++){
+	        System.out.print(x*i+" ");}
+	        System.out.println();
+	    }
+	  }
+	}
 
-}
