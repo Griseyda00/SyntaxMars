369becce70c218e570b1bab0752bf153970e8332
Griseyda00 <griseyda.09@gmail.com>
2020-03-17 20:31:13 -0400

    project and class created

--- src/test11/TestClass.java
+++ src/test11/TestClass.java
@@ -3,5 +3,6 @@ package test11;
 public class TestClass {
 public static void main(String[] args) {
 	System.out.println("Hello");
+	System.out.println();
 }
 }
--- src/test11/TestClass01.java
+++ src/test11/TestClass01.java
@@ -4,5 +4,6 @@ public class TestClass01 {
 public static void main(String[] args) {
 	System.out.println("helloooooooo");
 	System.out.println("helooolg");
+	System.out.println("Hello");
 }
 }
